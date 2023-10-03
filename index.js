bf0772ce8c66bf55d4abfcde36957d6ce16cb5c0
ammaralialis <ammaralialiwa@gmail.com>
2023-10-03 17:20:15 -0600

    create the authFIle and use it at app.js

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const dbConnection = require('./config/dbConnection');
 const app = express();
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 4000;
+const authRouter = require('./routes/authRoute');
 
 dbConnection()
 
 app.use('/',(req, res) =>{
     res.send('hello from the server side')
 })
+
+app.use('/api/user', authRouter)
+
 app.listen(PORT, ()=>{
     console.log(`server is running at PORT ${PORT}`);
 });
