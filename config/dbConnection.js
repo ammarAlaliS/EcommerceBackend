45eeff97cf66c5cbf7ba7fbfbea535eb7e66ffa6
ammaralialis <ammaralialiwa@gmail.com>
2023-10-03 17:05:28 -0600

    create the files to users and remove mongodb url

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose")
 
 const dbConnection = ()=>{
     try {
-        const conn = mongoose.connect('mongodb+srv://ammaralialiwa:JWksUR7DaKiApf34@cluster0.2pgu6ei.mongodb.net/?retryWrites=true&w=majority');
+        const conn = mongoose.connect(process.env.MONGODB_URL);
         console.log('connection success')
     } catch (error) {
        console.log('error to connect to data base')
