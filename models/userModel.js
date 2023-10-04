096133886a0152525dd94e425cfbb0d26fc09311
ammaralialis <ammaralialiwa@gmail.com>
2023-10-03 20:03:13 -0600

    install bcrypt dependency to use it at userModal file, this dependency is used to encryption password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose'); // Erase if already required
-
+const bcrypt = require('bcrypt');
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
     first_name:{
@@ -24,6 +24,11 @@ var userSchema = new mongoose.Schema({
         required:true,
     },
 });
+
+userSchema.pre('save',async function(next){
+    const salt = bcrypt.genSaltSync(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
 
 //Export the model
 module.exports = mongoose.model('User', userSchema);
