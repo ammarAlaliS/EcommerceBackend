ff3334d81fa3d061ea22a19d2eb1b562d20b62de
ammaralialis <ammaralialiwa@gmail.com>
2023-10-03 18:10:57 -0600

    make changes on index file and auth router

--- controller/userController.js
+++ controller/userController.js
@@ -2,17 +2,19 @@ const User = require('../models/userModel');
 
 const createUser = async (req, res ) =>{
     const email = req.body.email;
-    const findUser = await User.findOne(email);
+    const findUser = await User.findOne({ email: email});
     if (!findUser) {
         // CREATE A NEW USER
-        const newUser = User.create(req.body);
+
+        const newUser = await User.create(req.body);
         res.json(newUser);
+
     }else{
         // USER ALREADY EXISTS
         res.json({
             msg: "User Already Exist",
             success: "false",
-        })
+        });
     }
-}
-module.exports ={createUser}
+};
+module.exports ={ createUser };
